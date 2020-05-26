210be4f10c52b2a96b0e554e027b979e8b43e00a
uxsasukexnaruto <nguyha16@gmail.com>
2020-05-25 18:34:11 -0700

    seed db for postgres

---
+++ database/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE if exists info;
+
+CREATE DATABASE info;
+
+USE DATABASE info
+
+CREATE TABLE info (
+	city VARCHAR(100) NOT NULL,
+	title VARCHAR(100) NOT NULL,
+	hostImage VARCHAR(255) NOT NULL,
+	roomInfo VARCHAR(100) NOT NULL,
+	numberOfGuests INT NOT NULL,
+	numberOfBedrooms INT NOT NULL,
+	numberOfBeds INT NOT NULL,
+	numberOfBaths INT NOT NULL,
+	isSuperhost BOOLEAN,
+	isGreatLocation BOOLEAN,
+	isSparklingClean BOOLEAN,
+	isGreatCheckIn BOOLEAN,
+	isSelfCheckIn BOOLEAN,
+	roomDescription VARCHAR NOT NULL,
+	hasWiFi BOOLEAN,
+	hasEssentials BOOLEAN,
+	hasCable BOOLEAN,
+	hasLaptopSpace BOOLEAN,
+	hasHeating BOOLEAN,
+	hasKitchen BOOLEAN,
+	hasPillowsBlankets BOOLEAN,
+	sleepingArrangements_bedroom INT NOT NULL,
+	ID INT NOT NULL PRIMARY KEY
+);
+
+COPY info FROM './db.csv' DELIMITER ',' SCV HEADER;
