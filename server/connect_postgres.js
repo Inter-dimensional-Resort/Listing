ed65e39cc2d997bde105d65be8c66e717388d2b2
uxsasukexnaruto <nguyha16@gmail.com>
2020-06-01 11:51:25 -0700

    add env

--- server/connect_postgres.js
+++ server/connect_postgres.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 
 const pool = new Pool({
-  user: process.env.DB_USER,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
 
