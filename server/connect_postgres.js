2a281a14b3327fed211e083a12c1a98550dd4e40
uxsasukexnaruto <nguyha16@gmail.com>
2020-06-01 10:47:06 -0700

    fix dotenv

--- server/connect_postgres.js
+++ server/connect_postgres.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
-const auth = require('./.env');
+require('dotenv').config();
+
 
 const pool = new Pool({
-  user: auth.username,
-  host: 'localhost',
-  database: 'info',
-  password: auth.password,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
 });
 
 // async/await - check out a client
