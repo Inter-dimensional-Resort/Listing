1fee6f64bcd1a4ae81adb97e7eae951986edb587
uxsasukexnaruto <nguyha16@gmail.com>
2020-06-02 12:38:49 -0700

    refactor query info

--- server/connect_postgres.js
+++ server/connect_postgres.js
@@ -18,29 +18,7 @@ const getProduct = async (req, res) => {
   try {
     const data = await client.query('SELECT * FROM listing WHERE id = $1', [id]);
 
-    const amenity = {
-      basic: {
-        hasWiFi: true,
-        hasEssentials: true,
-        hasCable: true,
-        hasLaptopSpace: true,
-        hasHeating: true,
-      },
-      dining: {
-        hasKitchen: true,
-      },
-      bedAndBath: {
-        hasPillowsBlankets: true,
-      },
-    };
-
-
     const listing = data.rows[0];
-    const sleepingArrangements = {
-      bedroom: data.rows[0].numberofbedrooms,
-    };
-    listing.amenity = amenity;
-    listing.sleepingArrangements = sleepingArrangements;
     res.send(listing);
   } finally {
     // Make sure to release the client before any error handling,
